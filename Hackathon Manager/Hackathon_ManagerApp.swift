6c398100d93060c914768b62627c73cdae5e589b
ItsMeNoobieboy <54125080+ItsMeNoobieboy@users.noreply.github.com>
2024-06-08 19:01:57 -0400

    Initial Commit

---
+++ Hackathon Manager/Hackathon_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon_ManagerApp.swift
+//  Hackathon Manager
+//
+//  Created by Yancheng Zhao on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Hackathon_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
